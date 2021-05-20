e26e8a4ca81329f3664593eaccf9cbbb6db22f46
Soraya Benson <soraya.benson@gmail.com>
2021-05-20 12:51:48 -0700

    list styling

--- src/components/villagers/VillagerItem.jsx
+++ src/components/villagers/VillagerItem.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import style from './villager-list.css';
 
 const VillagerItem = ({ id, image, name, catchphrase }) => {
     return (
     
-    <Link to={`/${id}`}>
+    <Link 
+        to={`/${id}`}
+        className={style.villagerItem}>
         <img src={image} alt={name} />
         <h3 aria-level='3'>{name}</h3>
         <p aria-label='villager catchphrase'>{catchphrase}</p>
